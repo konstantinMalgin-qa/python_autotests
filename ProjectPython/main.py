e826fdbaf335138a8a42c9adbebec820dd27c61a
Konstantin <137667593+konstantinMalgin-qa@users.noreply.github.com>
2023-08-10 13:21:45 +0500

    Update main.py

--- ProjectPython/main.py
+++ ProjectPython/main.py
@@ -3,47 +3,47 @@ import requests
 token = '7f98023761a246726db06eecb07cf32b'
 host = 'https://api.pokemonbattle.me:9104' # хост для покемонов
 
-'''response = requests.post('https://api.pokemonbattle.me:9104/trainers/reg',json = {
+response = requests.post('https://api.pokemonbattle.me:9104/trainers/reg',json = {
     "trainer_token": token,
     "email": "kostayyqa@dolnikov.ru",
     "password": "Iloveqa1"
 }, headers = {'Content-Type':'application/json'})
 
-print(response.text)'''
+print(response.text)
 
-'''response_activation = requests.post(f'{host}/trainers/confirm_email', json = {
+response_activation = requests.post(f'{host}/trainers/confirm_email', json = {
     "trainer_token": token
 }, headers = {'Content-Type':'application/json'})
 
-print(response_activation.text)'''
+print(response_activation.text)
 
-'''response_pokemons = requests.post(f'{host}/pokemons', json = {
+response_pokemons = requests.post(f'{host}/pokemons', json = {
     "name": "luma",
     "photo": "https://dolnikov.ru/pokemons/albums/001.png"
 }, headers = {'Content-Type':'application/json','trainer_token': token})
 
-print(response_pokemons.text)'''
+print(response_pokemons.text)
 
-'''response_pokemons = requests.post(f'{host}/pokemons', json = {
+response_pokemons = requests.post(f'{host}/pokemons', json = {
     "name": "puma",
     "photo": "https://dolnikov.ru/pokemons/albums/001.png"
 }, headers = {'Content-Type':'application/json','trainer_token': token})
 
-print(response_pokemons.text)'''
+print(response_pokemons.text)
 
-'''response_change_pokemons = requests.put(f'{host}/pokemons', json = {
+response_change_pokemons = requests.put(f'{host}/pokemons', json = {
     "pokemon_id": "5618",
     "name": "zuma",
     "photo": "https://dolnikov.ru/pokemons/albums/008.png"
 
 }, headers = {'Content-Type':'application/json','trainer_token': token})
 
-print(response_change_pokemons.text)'''
+print(response_change_pokemons.text)
 
-'''response_add_pokemons = requests.post(f'{host}/trainers/add_pokeball', json = {
+response_add_pokemons = requests.post(f'{host}/trainers/add_pokeball', json = {
     "pokemon_id": "5618"
 
 }, headers = {'Content-Type':'application/json','trainer_token': token})
 
-print(response_add_pokemons.text)'''
+print(response_add_pokemons.text)
 
