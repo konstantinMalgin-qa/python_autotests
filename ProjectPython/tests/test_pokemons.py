4738cb7e289de36084452f7fa9938044dc9950fa
Konstantin <137667593+konstantinMalgin-qa@users.noreply.github.com>
2023-08-10 14:02:01 +0500

    Update test_pokemons.py

--- ProjectPython/tests/test_pokemons.py
+++ ProjectPython/tests/test_pokemons.py
@@ -7,3 +7,9 @@ token = '7f98023761a246726db06eecb07cf32b'
 def test_status_code():
     response = requests.get(f'{host}/trainers', params= {'trainer_id' : 1866})
     assert response.status_code ==200
+
+def test_part_of_answer():
+    response = requests.get(f'{host}/trainers', params= {'trainer_id' : 1866})
+   
+    
+    assert response.json()['trainer_name'] == 'Kostya'
